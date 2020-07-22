920369e27709330a3c2705d37dd7705ee3526642
Siamen Paliashchuk <sempol777@gmail.com>
2020-07-21 20:37:47 -0400

    added animation and background

---
+++ script.js
@@ -0,0 +1,12 @@
+//removing presentation
+
+function myFunction() {
+    setTimeout(() => {
+        var element = document.getElementById("pr");
+         element.style.display = "none";
+       
+    
+    }, 4000);
+  }
+
+  myFunction();
